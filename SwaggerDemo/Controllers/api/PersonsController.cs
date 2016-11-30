a74160e1c237e1e347199fa38cea38c5966c1187
Gene Vangampelaere <gene.vangampelaere@outlook.com>
2016-11-30 08:34:06 +0100

    ok

---
+++ SwaggerDemo/Controllers/api/PersonsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SwaggerDemo.Controllers.api
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PersonsController : ApiController
+    {
+        // GET: api/Persons
+        public IEnumerable<Person> Get()
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person() { Name = "Gene", Email = "gene.vangampelaere@howest.be" });
+            persons.Add(new Person() { Name = "Jef", Email = "jef.daels@howest.be" });
+            persons.Add(new Person() { Name = "Simon", Email = "simon.rosseel@howest.be" });
+
+            return persons;
+        }
+
+
+        
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+}
